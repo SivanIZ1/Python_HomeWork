1aa670ffa431b41bcd18cb3b45165b21c390e2b2
Siavn <siviz1411@gmail.com>
2026-05-31 19:44:48 +0300

    HomeWork47

---
+++ HomeWork_47.py
@@ -0,0 +1,6 @@
+my_list = [10, 20, 30, 40, 50]
+print("האיבר הראשון:", my_list[0])
+print("האיבר האחרון:", my_list[-1])
+list_length = len(my_list)
+my_list.append(list_length)
+my_list.append(list_length)
